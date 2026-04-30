a5a2c95ac7dfcc52ccabb6d26d7b661b4b2747e6
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-04-30 09:42:07 -0300

    add - tentei arrumar oque esta dando erro

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let cadastro = false;
 document.getElementById("toggle").onclick = () => {
     cadastro = !cadastro;
     document.getElementById("titulo").innerText = cadastro ? "Cadastro" : "Login";
-    document.querySelector("button").innerText = cadastro ? "Cadastrar" : "Entrar";
-    document.getElementById("toggle").innerText = cadastro ? "Já tem conta? Faça Login!" : "Não tem conta? Cadastre-se!";
+    document.getElementById("botao").innerText = cadastro ? "Cadastrar" : "Entrar";
+    document.getElementById("toggle").innerHTML = cadastro "Já tem conta? Faça Login!" : "Não tem conta? Cadastre-se!";
     document.getElementById("mensagem").innerHTML = "";
 }
 
